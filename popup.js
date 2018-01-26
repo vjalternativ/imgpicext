a333a796ad17771a3915071e9f7c10daae5b8813
root <vj.alternativ@gmail.com>
2018-01-26 10:37:18 +0530

    changes

--- popup.js
+++ popup.js
@@ -1,42 +1,60 @@
+var imgpicExtBaseUrl = "https://imgpic.org/";
+var imgpicExtUsername  = "";
+var imgpicExtPassword = "";
+
 function getImgPicSession() {
 	var username = prompt("Username");
 	var password = prompt("password");
-	//var url = "http://localhost/imgpic/chrome/login";
-    var url = "https://imgpic.org/chrome/login";
+    var url = imgpicExtBaseUrl+"chrome/login";
     var data = {imgpic_chorme_username:username,imgpic_chrome_password:password};
 	$.post(url,data,function(res){
 	var result = JSON.parse(res);
 	if(result.status=="pass") {
-		chrome.storage.sync.set({'imgpic_session_id': result.session_id}, function() {
-			// Notify that we saved.
-			//console.log(result.session_id);
-			//console.log('Settings saved');
+		imgpicExtUsername = result.username;
+		imgpicExtPassword = result.password;
+		chrome.storage.sync.set({'imgpic_session_id': result.session_id,'imgpic_ext_username':result.username,'imgpic_ext_password':result.password}, function() {
+			
+			initImgPicExt(result.session_id);
+			
 		  });
-		//$("body").append('<hr/><h1>'+result.session_id+'</h1>');
-		//$("body").append('<script src="http://localhost/imgpic/webapp/js/chrome.js" ></script>');
 	}
 });
 }
 
-function imgpicShowSave() {
-	console.log("test");
+
+function initImgPicExt(session_id) {
+	$("body").append('<script>var imgpicExtBaseUrl="'+imgpicExtBaseUrl+'";</script>');
+	$("body").append('<script>var imgpic_session_id="'+session_id+'";</script>');
+	$("body").append('<script>var imgpic_ext_username="'+imgpicExtUsername+'";</script>');
+	$("body").append('<script>var imgpic_ext_password="'+imgpicExtPassword+'";</script>');
+	$("body").append('<div id="imgpic_ext_chrome"></div>');
+	//$("body").append('<script id="imgpic_ext_chrome_script"></script>');
+	$.getScript( chrome.extension.getURL("test.js") );	
+	$("#imgpic_ext_chrome").load(chrome.extension.getURL("chrome.html"));
+	
 }
 
+
 function validateImgpicSession(sessionId) {
-	//var url = "http://localhost/imgpic/chrome/validateSession";
-	var url = "https://imgpic.org/chrome/validateSession";
+	var url = imgpicExtBaseUrl+"chrome/validateSession";
 	$.post(url,{imgpic_session_id:sessionId},function(res){
+		console.log("Validating imgpic sesison");
 		console.log(res);
 		var result = JSON.parse(res);
 		if(result.status=="fail") {
 			getImgPicSession();
+		} else {
+			initImgPicExt(sessionId);
 		}
 	});
 
 }
-chrome.storage.sync.get('imgpic_session_id', function(items) {
-	console.log('Settings retrieved');
-	console.log( items.imgpic_session_id);
+chrome.storage.sync.get(['imgpic_session_id','imgpic_ext_username','imgpic_ext_password'], function(items) {
+
+	imgpicExtUsername = items.imgpic_ext_username;
+	imgpicExtPassword = items.imgpic_ext_password;
+
+
 	if(!items.hasOwnProperty("imgpic_session_id") || items.imgpic_session_id=='') {
 		getImgPicSession();
 	} else {
@@ -44,46 +62,3 @@ chrome.storage.sync.get('imgpic_session_id', function(items) {
 	}
 
 });
-//var html = load(chrome.extension.getURL("chrome.html"));
-  
-   //console.log(html);
-	$("body").append('<div id="imgpic_ext_chrome"></div>');
-	//$("body").append('<script id="imgpic_ext_chrome_script"></script>');
-	$.getScript( chrome.extension.getURL("test.js") );	
-	$("#imgpic_ext_chrome").load(chrome.extension.getURL("chrome.html"));	
-//chromeStoragetest() ;
-//$("body").append('<script src="http://localhost/imgpic/webapp/js/chrome.js" ></script>');
-
-/*
-$("body").append('<script src="https://imgpic.org/webapp/js/jquery-3.0.0.min.js" ></script>');
-$("body").append('<script src="https://imgpic.org/webapp/js/bootstrap.min.js"></script>');
-$("body").append('<script>var base_url ="https://imgpic.org/";</script>');
-$("body").append('<script src="https://imgpic.org/webapp/js/feeds.js"></script>');
-$("body").a)ppend('<div id="imgpicmodal"></div>');
-$("body").append('<script type="text/javascript" >'+
-'function showboardmodal(src,e){'+
-'e.preventDefault();'+
-'$.ajaxSetup({ crossDomain: true,xhrFields: { withCredentials: true}});'+
-'$("#imgpicsavebtn").remove();var url = "https://imgpic.org/ip_user/ajaxAddToBoardModal";'+
-				'$.post(url,{},function(result){'+
-				'$("#imgpicmodal").html(result);webtitle = $(location).attr("href"); pinimage(src,webtitle);'+
-				'});}</script>');
-
-
-$("img").hover(function(e){
-						//e.preventDefault();
-						var src = this.src;
-	$(".imgpicsavebtn").remove();
-    $(this).parent().attr("href","javascript:");
-	var html = '<span id="imgpicsavebtn" class="imgpicsavebtn" style="position:absolute;z-index:8975309;width:auto;padding:10px;color:#fff;border:1px solid rgb(128,128,128);border-radius:4px;margin:6px;background-color:#0084b4;cursor:pointer;" onclick="showboardmodal(\''+src+'\',event);return false;">Save</span>';
-	$(this).before(html);
-});
-
-
-$(".imgpicsavebtn").hover(function(){						
-	}, function(){
-	$(".imgpicsavebtn").remove();
-});
-
-
-*/
--- test.js
+++ test.js
@@ -1,15 +1,170 @@
+HTMLCollection.prototype.forEach = Array.prototype.forEach;
+NodeList.prototype.forEach = Array.prototype.forEach;
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
 function imgpicShowSave() {
-   var images = document.images;
-   for(var i=0;i<images.length;i++) {
-       console.log(images[i]);
-       images[i].onmouseover = function() {
-           showBtn(images[i]);
-       };
-   }
+    var buttons = document.getElementsByClassName("imgpic_save_btn_container");
+    buttons.remove();
+    var images = document.images;
+    images.forEach(function(element, i){
+        showImgPicSaveBtn(element);
+    });    
 }
 
-function showBtn(ele) {
-    var text = document.createTextNode("my text must be added here.");
+function showImgPicSaveBtn(ele) {
+    var container = document.createElement("span");
+    container.id = "imgpic_save_btn_container";
+    container.classList.add("imgpic_save_btn_container");
+    var btn = document.createElement("button");        
+    btn.id="imgpic_save_btn";
+    btn.class = "imgpic_save_btn";
+    btn.innerHTML = "Save";
+    btn.onclick = function() { showImgpicExtModal(event,ele); };
+    container.appendChild(btn);
+    ele.parentNode.insertBefore(container, ele.nextSibling);
+    if(typeof ele.parentNode.href !="undefined") {
+        //ele.parentNode.href="javascript:";
+        //ele.parentNode.removeAttribute("jsaction");
+    }
+    ele.removeAttribute("jsaction");
+}
+
+function resizeDcsModal(id) {
+    var modalele = document.getElementById(id);
+    var windowwidth = window.innerWidth;
+    var modalwidth = windowwidth*0.6;
+    var left  = (windowwidth / 2) - (modalwidth / 2);
+    modalele.style.left = left+"px";
+}
+
+function imgpicAddClass(id,className) {
+    element = document.getElementById(id);
+    element.classList.add(className);
+}
+function imgpicRemoveClass(id,className) {
+    element = document.getElementById(id);
+    element.classList.remove(className);
+}
+function initDcsModal(id) {
+    resizeDcsModal(id);
+    imgpicRemoveClass(id,"hide");
     
-    ele.parentNode.insertBefore(text, ele.nextSibling);
+}
+
+var imgpicExtBoards = '';
+
+function  imgpicGenerateFormData(object) {
+    object.imgpic_user_name = imgpic_ext_username;
+    object.imgpic_password = imgpic_ext_password;
+    
+    data = new FormData();
+    for (var property in object) {
+        if (object.hasOwnProperty(property)) {
+            // do stuff
+            data.append( property, object[property] );
+        }
+    }
+    return data;
+}
+
+
+var imgpicExtBoardList ='';
+function showImgpicExtModal(e,ele) {
+    e.preventDefault();
+    src = ele.src;
+    var website = document.URL;
+    var parentNode = ele.parentNode;
+    href = parentNode.href;
+    if(typeof href !='undefined') {
+        var strArray = href.split("imgurl=");
+        src = ele.src;
+        if(strArray.length>=1) {
+            var str = strArray[1];
+            if(typeof str!="undefined") {
+                strArray = str.split("&imgrefurl=");
+                if(strArray.length >=1) {
+                    src = strArray[0];
+                    src = unescape(src);
+                        str = strArray[1];
+                        strArray = str.split("&docid");
+                        if(strArray.length>=1) {
+                            website = strArray[0];
+                            website = unescape(website);
+                        }
+                }
+            }
+        }
+    }
+    document.getElementById("imgpicExtWebsite").value=website;
+    document.getElementById("imgpicExtModalImage").src=src;
+    var url = imgpicExtBaseUrl+"chrome/getUserBoards";
+    var data = imgpicGenerateFormData({imgpic_session_id:imgpic_session_id});
+    if(imgpicExtBoardList!="") {
+        initDcsModal('imgpicExtSaveModal');
+        return;
+    }
+    imgpicAjaxCall(url,data,function(res){
+        var result = JSON.parse(res);
+        if(result.status=="fail") {
+            imgpicExtReload();
+        } else {
+            imgpicExtBoardList =result.html;
+             document.getElementById("imgpicExtBoards").innerHTML= result.html;   
+             initDcsModal('imgpicExtSaveModal');
+        }
+        
+    });
+    
+}
+
+function imgpicExtReload() {
+    var x = confirm("ImgPic Session Expired. Do you want to reload the page?");
+            if(x) {
+                  window.location.reload();
+                  return false;  
+            }
+}
+
+function imgpicAjaxCall(url,data,callback) {
+    //document.cookie = "PHPSESSID="+imgpic_session_id;
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+         callback(this.responseText);
+        }
+      };
+    xmlhttp.open("POST", url);
+    xmlhttp.send(data);
+}
+
+
+function imgpicExtSaveBoard(btn,bid) {
+        btn.disabled=true;
+		var url = imgpicExtBaseUrl+"chrome/saveImageToBoard";
+		var data = {};
+		data.imgsrc =  document.getElementById("imgpicExtModalImage").src;
+		data.website = document.getElementById("imgpicExtWebsite").value;
+		data.boardid = bid;
+        data.description = document.getElementById("imgpicExtModalDesc").value;
+        
+        var formdata = imgpicGenerateFormData(data);
+        imgpicAjaxCall(url,formdata,function(res){
+                var result = JSON.parse(res);
+                if(status=='fail') {
+                    imgpicExtReload();
+                } else {
+                    imgpicAddClass("imgpicExtSaveModal","hide");
+                    btn.disabled=false;
+		        }
+        });
+        
+        
 }
